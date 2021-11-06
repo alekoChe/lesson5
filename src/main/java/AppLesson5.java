5080321da6c4d826f4cd7da1b532cad221f7e4b5
alekoChe <aleko2117bis@gmail.com>
2021-11-06 13:34:30 +0300

    чистовой вариант

--- src/main/java/AppLesson5.java
+++ src/main/java/AppLesson5.java
@@ -1,6 +1,6 @@
 public class AppLesson5 {
 
-    static final int SIZE = 10_000_000;
+    static final int SIZE = 100_000_000;
     static final int HALF = SIZE / 2;
     float[] arr = new float[SIZE];
 
@@ -8,27 +8,64 @@ public class AppLesson5 {
         this.arr = arr;
     }
 
+    public static float calculator(float number, int i) {
+        return (float)(number * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    }
+
     public static void main(String[] args) {
         AppLesson5 app = new AppLesson5();
         firstMethod(app.arr);  // 1134, 1132, 1132
+        secondMethod(app.arr);
     }
     public static void firstMethod(float[] arr) {
-        /**   2) Заполняют этот массив единицами.
-              3) Засекают время выполнения: long a = System.currentTimeMillis().
-              4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
-         arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-         */
+
         for (int i = 0; i < SIZE; i++) {
             arr[i] = 1.0f;
         }
         long a = System.currentTimeMillis();
         for (int i = 0; i < SIZE; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            //arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            arr[i] = calculator(arr[i], i);
         }
-        /** 5) Проверяется время окончания метода System.currentTimeMillis().
-         6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a).
-         */
+
         System.currentTimeMillis();
+        System.out.println("a= " + a);
         System.out.println("One thread time: " + (System.currentTimeMillis() - a) + " m.s.");
+    }
+    public static void secondMethod(float[] arr) {
+       
+         for (int i = 0; i < SIZE; i++) {
+            arr[i] = 1.0f;
+        }
+        long a = System.currentTimeMillis();
+        float [] leftArr = new float[HALF];
+        float [] rightArr = new float[HALF];
+        System.arraycopy(arr, 0, leftArr, 0, leftArr.length);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < leftArr.length; i++) {
+                    leftArr[i] = calculator(leftArr[i], i);
+                }
+            }
+        });
+        thread1.start();
+
+        System.arraycopy(arr, HALF, rightArr, 0, HALF);
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0 ; i < rightArr.length; i++) {
+                    rightArr[i] = calculator(rightArr[i], i + HALF);
+                }
+            }
+        });
+        thread2.start();
+
+        float [] mergedArray = new float[SIZE];
+        System.arraycopy(leftArr, 0, mergedArray, 0, leftArr.length);
+        System.arraycopy(rightArr, 0, mergedArray, rightArr.length, rightArr.length);
+        System.currentTimeMillis();
+        System.out.println("Two thread time: " + (System.currentTimeMillis() - a) + " m.s.");
     }
 }
