6137abf6ef6dc27158994d3925950e5e9d2494c4
alekoChe <aleko2117bis@gmail.com>
2021-11-08 09:40:00 +0300

    чистовой вариант 3, добавил метод с 4-мя потоками

--- src/main/java/AppLesson5.java
+++ src/main/java/AppLesson5.java
@@ -16,6 +16,8 @@ public class AppLesson5 {
         AppLesson5 app = new AppLesson5();
         firstMethod(app.arr);  // 1134, 1132, 1132
         secondMethod(app.arr);
+        System.out.println("///////////////////////////////////////////////////////////");
+        app.run();
     }
     public static void firstMethod(float[] arr) {
 
@@ -24,7 +26,6 @@ public class AppLesson5 {
         }
         long a = System.currentTimeMillis();
         for (int i = 0; i < SIZE; i++) {
-            //arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             arr[i] = calculator(arr[i], i);
         }
 
@@ -33,7 +34,7 @@ public class AppLesson5 {
         System.out.println("One thread time: " + (System.currentTimeMillis() - a) + " m.s.");
     }
     public static void secondMethod(float[] arr) {
-
+        /** метод с разделением и последующей склейкой массива (по условию задания) */
          for (int i = 0; i < SIZE; i++) {
             arr[i] = 1.0f;
         }
@@ -41,26 +42,35 @@ public class AppLesson5 {
         float [] leftArr = new float[HALF];
         float [] rightArr = new float[HALF];
         System.arraycopy(arr, 0, leftArr, 0, leftArr.length);
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < leftArr.length; i++) {
-                    leftArr[i] = calculator(leftArr[i], i);
-                }
+//        Thread thread1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < leftArr.length; i++) {
+//                    leftArr[i] = calculator(leftArr[i], i);
+//                }
+//            }
+//        });
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < leftArr.length; i++) {
+                leftArr[i] = calculator(leftArr[i], i);
             }
         });
-        thread1.start();
 
         System.arraycopy(arr, HALF, rightArr, 0, HALF);
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0 ; i < rightArr.length; i++) {
-                    rightArr[i] = calculator(rightArr[i], i + HALF);
-                }
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0 ; i < rightArr.length; i++) {
+                rightArr[i] = calculator(rightArr[i], i + HALF);
             }
         });
+
+        thread1.start();
         thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         float [] mergedArray = new float[SIZE];
         System.arraycopy(leftArr, 0, mergedArray, 0, leftArr.length);
@@ -68,4 +78,46 @@ public class AppLesson5 {
         System.currentTimeMillis();
         System.out.println("Two thread time: " + (System.currentTimeMillis() - a) + " m.s.");
     }
+    public  void run() {
+        /** Метод одновременной обработки одного массива разными потоками (как на лекции) */
+        AppLesson5 app = new AppLesson5();
+        ThreadCalculator calc1 = new ThreadCalculator(app.arr, 0, SIZE / 4);
+        ThreadCalculator calc2 = new ThreadCalculator(app.arr, SIZE / 4, SIZE / 2);
+        ThreadCalculator calc3 = new ThreadCalculator(app.arr, SIZE / 2, (SIZE / 2 + SIZE / 4));
+        ThreadCalculator calc4 = new ThreadCalculator(app.arr, (SIZE / 2 + SIZE / 4), SIZE);
+        long a = System.currentTimeMillis();
+        calc1.start();
+        calc2.start();
+        calc3.start();
+        calc4.start();
+
+        try {
+            calc1.join();
+            calc2.join();
+            calc3.join();
+            calc4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Four thread time: " + (System.currentTimeMillis() - a) + " m.s.");
+    }
+    public class ThreadCalculator extends Thread{
+        int start;
+        int finish;
+        float[] arr;
+
+        public ThreadCalculator(float[] arr, int start, int finish) {
+            this.arr = arr;
+            this.start = start;
+            this.finish = finish;
+        }
+
+        @Override
+        public void run() {
+            for (int i = start; i < finish; i++) {
+                arr[i] = AppLesson5.calculator(arr[i], i);
+            }
+        }
+    }
+
 }
